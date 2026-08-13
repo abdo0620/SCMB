bfa2072e28693b2e39e72fea479aadeb02e96905
a25begdo <abdel-moujib.begdouri-terraf@imt-atlantique.net>
2026-08-13 20:23:52 +0200

    feat: update model definition to include weights and add data augmentation

--- src/Model.py
+++ src/Model.py
@@ -1,13 +1,21 @@
 import tensorflow.keras as tf
 import tensorflow.keras.layers as tfl
 from tensorflow.keras.applications import MobileNetV2
-import matplotlib.pyplot as plt
+from tensorflow.keras.applications.mobilenet_v2 import preprocess_input
+import numpy as np
+
+import dataset
 IMG_SIZE=(224,224,3)
-
-base_model=MobileNetV2(include_top=False,input_shape=IMG_SIZE)
+base_model=MobileNetV2(include_top=False,input_shape=IMG_SIZE,weights="imagenet")
 base_model.trainable=False
-model=tf.models.Sequential([base_model,tfl.GlobalAveragePooling2D(), tfl.Dense(128,activation="relu"),tfl.Dense(1,activation="sigmoid")])
 
+print(len(base_model.layers))
+print(base_model.summary())
+def detect(img_size=IMG_SIZE, augmenter=dataset.data_augmenter):
+    model=tf.models.Sequential([augmenter(),tfl.Lambda(preprocess_input),base_model,tfl.GlobalAveragePooling2D(), tfl.Dense(128,activation="relu"),tfl.Dense(1,activation="sigmoid")])
+    return model 
+model=detect()
 
-
+print(len(model.layers))
 print(model.summary())
+print(((model.weights)))
